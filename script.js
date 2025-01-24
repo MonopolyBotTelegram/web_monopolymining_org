bfa59d4c11b23b9e58edcc53752f469541b62e92
MonopolyBotTelegram <monopolybottelegram@gmail.com>
2025-01-24 10:42:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Ejemplo de animación suave al hacer scroll hacia una sección
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
